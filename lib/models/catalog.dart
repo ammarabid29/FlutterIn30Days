953cf73f31d561b8f800b5cf09015d94cce2cccd
ammarabid29 <aammarawan786@gmail.com>
2023-08-25 14:53:33 +0500

    day 12 listview

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,5 +1,19 @@
+class CatalogModel {
+  static final items = [
+    Item(
+      id: 1,
+      name: "iPhone 12 Pro",
+      price: 999,
+      desc: "Apple iPhone 12th Generation",
+      image:
+          "https://images.unsplash.com/photo-1591337676887-a217a6970a8a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGlwaG9uZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+      color: "#33505a",
+    )
+  ];
+}
+
 class Item {
-  final String id;
+  final int id;
   final String name;
   final num price;
   final String desc;
@@ -15,15 +29,3 @@ class Item {
     required this.color,
   });
 }
-
-final prodcuts = [
-  Item(
-    id: "Codepur001",
-    name: "iPhone 12 Pro",
-    price: 999,
-    desc: "Apple iPhone 12th Generation",
-    image:
-        "https://images.unsplash.com/photo-1591337676887-a217a6970a8a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGlwaG9uZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
-    color: "#33505a",
-  )
-];
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
+import 'package:flutter_catalog/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    const int days = 30;
-    const String name = "Codepur";
+    final dummyList = List.generate(50, (index) => CatalogModel.items[0]);
 
     return Scaffold(
       appBar: AppBar(
@@ -15,25 +16,15 @@ class HomePage extends StatelessWidget {
           "Catalog App",
         ),
       ),
-      body: Center(
-          child: Container(
-        constraints: const BoxConstraints(
-          minHeight: 70,
-          maxHeight: 200,
-          minWidth: 70,
-          maxWidth: 200,
-        ),
-        child: Container(
-          color: Colors.red,
-          height: 200,
-          width: 100,
-        ),
-      )
-          // Text(
-          //   'Welcome to $days days of flutter by $name',
-          // ),
-
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) => ItemWidget(
+            item: dummyList[index],
           ),
+        ),
+      ),
       drawer: const MyDrawer(),
     );
   }
